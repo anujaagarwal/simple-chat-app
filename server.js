97f33841f3f617ea5104ac52251e25b59f9ce3d6
anujaAgarwal1 <anuja@stockgro.com>
2023-08-17 08:40:21 +0530

    First Commit

---
+++ server.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const http = require('http');
+const socketIO = require('socket.io');
+
+const app = express();
+const server = http.createServer(app);
+const io = socketIO(server);
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', (socket) => {
+    console.log('A user connected');
+
+    socket.on('joinRoom', (room) => {
+        socket.join(room);
+    });
+
+    socket.on('message', (data) => {
+        io.to(data.room).emit('message', data.message);
+    });
+
+    socket.on('disconnect', () => {
+        console.log('A user disconnected');
+    });
+});
+
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
