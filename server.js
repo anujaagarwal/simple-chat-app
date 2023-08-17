2b1679e35e124de54288a5f1855beb9edd635b55
anujaAgarwal1 <anuja@stockgro.com>
2023-08-17 19:01:22 +0530

    commit 3

--- server.js
+++ server.js
@@ -1,14 +1,17 @@
 const express = require('express');
 const http = require('http');
+const path = require('path'); 
 const socketIO = require('socket.io');
 
 const app = express();
 const server = http.createServer(app);
 const io = socketIO(server);
 
-app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/index.html');
-});
+// app.get('/', (req, res) => {
+//     res.sendFile(__dirname + '/public/index.html');
+// });
+
+app.use(express.static(path.join(__dirname, 'public')));
 
 io.on('connection', (socket) => {
     console.log('A user connected');
@@ -18,7 +21,7 @@ io.on('connection', (socket) => {
     });
 
     socket.on('message', (data) => {
-        io.to(data.room).emit('message', data.message);
+        io.to(data.room).emit('message', { message: data.message, sender: socket.id });
     });
 
     socket.on('disconnect', () => {
